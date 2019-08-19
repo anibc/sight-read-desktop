6fcb67990740c06f1b6be009c51c69c26cbd2e61
another <anibc@gmail.com>
2019-08-19 20:19:15 +0600

    initialize sample mode and register callback

--- sightread/mode.py
+++ sightread/mode.py
@@ -1,6 +1,8 @@
 from sightread import midi
+from sightread.ui import sheetwidget
 class MiniRandom( midi.input.MIDIListener ):
     def __init__( self, sw ):
-        pass
+        self.sw = sheetwidget.StaticSheetWidget( sw )
+        midi.input.register( self )
     def on_midi_input( self, msg ):
         pass
