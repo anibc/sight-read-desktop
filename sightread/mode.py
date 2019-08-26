a6a625375986eadce41d6805c5943d75d1331237
another <anibc@gmail.com>
2019-08-26 22:22:52 +0600

    fix typo staticsheetwidget to staticsheet

--- sightread/mode.py
+++ sightread/mode.py
@@ -1,4 +1,4 @@
 from sightread.ui import sheetwidget
 class MiniRandom():
     def __init__( self, sw ):
-        self.sw = sheetwidget.StaticSheetWidget( sw )
+        self.sw = sheetwidget.StaticSheet( sw )
