29b51dd7bb66a445cb307a06134dbe7565a7e459
another <anibc@gmail.com>
2019-09-22 22:39:27 +0600

    implemented demo fixed sheet lines

--- sightread/ui/sheetwidget.py
+++ sightread/ui/sheetwidget.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QWidget
 import PyQt5.QtGui as QtGui
 from sightread import mode
 from sightread.midi.input import MIDIListener, register, deregister
+from sightread import note
 
 class SheetWidget(QWidget):
     def __init__(self):
@@ -32,11 +33,13 @@ class SheetWidget(QWidget):
     def drawWidget( self, qp ):
         qp.setPen(QtGui.QColor(255, 150, 150))
         qp.setBrush(QtGui.QColor(150, 255, 150))
-        # width = size().width()
+        width = self.size().width()
+        height = self.size().height()
         qp.drawText(40,40, "Line of Text")
         # qp.drawGlyphRun();
-        for i in range( 5 ):
-            qp.drawLine( i * 10 + 50, 20, i * 10 + 50, 50 )
+        for i in range( note.SHEETLOW.n8, note.SHEETHIGH.n8 + 1, 2 ):
+            y = i * 2 + 10
+            qp.drawLine( 0, y, width - 10, y)
 
 class SheetController():
     def __init__( self, sw ):
