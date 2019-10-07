a66709771f768123d2df6bc8746d9059f8c36088
another <oni74mail@gmail.com>
2019-10-07 23:48:31 +0600

    created viewable notes heap

--- sightread/viewablenotes.py
+++ sightread/viewablenotes.py
@@ -1,11 +1,18 @@
 from sightread.note import Note
 class ViewableNote( Note ):
-    def __init__( self, n, t ):
+    def __init__( self, n, st, et ):
         super().__init__()
-        self.t = t
+        self.n = n
+        self.st = st
+        self.et = et
+
 class NoteModel:
     def __init__( self ):
         self.l = []
+        self.incoming  = MinHeap( lambda n: n.st )
+        self.windowIn  = MaxHeap( lambda n: n.st )
+        self.windowOut = MinHeap( lambda n: n.et )
+        self.outgoing  = MaxHeap( lambda n: n.et )
     def insert( self, vn ):
         pass
     def remove( self, vn ):
@@ -31,3 +38,25 @@ class NoteModel:
                 j += m
             m //= 2
         return i + 1, j - 1
+
+class MinHeap(Heap):
+    def cmp( self, a, b ):
+        return self.key( a ) < self.key( b )
+
+class MaxHeap(Heap):
+    def cmp( self, a, b ):
+        return self.key( a ) > self.key( b )
+
+class Heap:
+    def __init__( self, key ):
+        self.key = key
+        self.l = []
+        self.k = {}
+    def insert( self, n ):
+        pass
+    def remove( self, n ):
+        pass
+    def top( self, n ):
+        pass
+    def pop( self, n ):
+        pass
