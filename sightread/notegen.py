adbba5c54ce61e3f21a42c959f2b51c6324764d2
anibc <oni74mail@gmail.com>
2020-05-14 22:08:13 +0600

    created notegen package

---
+++ sightread/notegen.py
@@ -0,0 +1,21 @@
+"""
+Random or serial note/chord generators
+
+inputs:
+    random?
+    notes or chords?
+    note range minimum
+    note range maximum
+    note from particular scale(s)
+    note from particular set of notes   #not as important
+    for serial generator, SCAN or CSCAN?
+    serially read notes from midi
+"""
+
+# Always return list of notes
+
+def gen_note():
+    pass
+
+def gen_chord():
+    pass
