bafdc87a0455d560f51d11913991afb633e096d6
anibc <oni74mail@gmail.com>
2020-08-23 16:53:55 +0600

    implemented everything in NoteModel but map from x to t

--- sightread/notemodel.py
+++ sightread/notemodel.py
@@ -11,20 +11,47 @@ class TimeSignature:
 
 class NoteModel:
     def __init__(self, timesig=TimeSignature()):
-        self.measures = []
         self.timesignature = timesig
 
+    @property
+    def measures(self):
+        raise NotImplemented
+
     def timeToX(self, time, bpm):
-        "maps time in given bpm to x for NoteModel"
-        pass
+        "maps time for given bpm to x in NoteModel"
+        beats = self.timesignature.beats
+        width = beats * XPerBeat
+        fullWidth = width + XPerBeat
+        bps = bpm / 60
+        b = time * bps
+        measure = b // beats
+        b -= measure * beats
+        lastXInMeasure = self.measures[measure].lastX
+        xInMeasure = b / width
+        prevX = measure * fullWidth + XPerBeat
+        if xInMeasure < lastXInMeasure:
+            return prevX + xInMeasure
+        xInMeasure -= lastXInMeasure
+        xInMeasure /= width - lastXInMeasure
+        xInMeasure *= width - lastXInMeasure + XPerBeat
+        xInMeasure += lastXInMeasure
+        return prevX + xInMeasure
+
+    def xToTime(self, x, bpm):
+        "maps x to time for given bpm in NoteModel"
+        raise NotImplemented
 
     def range(self, l, r):
         "returns NoteRange from x value l to x value r"
-        pass
-    
+        raise NotImplemented
+
+    def barlines(self, l, r):
+        "returns list of x values at which bar lines must be drawn"
+        raise NotImplemented
+
     def appendNextBeat(self):
         "appends note as viewablenote in next available beat"
-        pass
+        raise NotImplemented
 
 class TransientNoteModel(NoteModel):
     pass
