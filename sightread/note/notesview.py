34502e836153e685c885e5dc93baeca949d3bfbd
anibc <oni74mail@gmail.com>
2020-11-06 00:34:40 +0600

    simplify notesview constructor

--- sightread/note/notesview.py
+++ sightread/note/notesview.py
@@ -1,13 +1,14 @@
 class NotesView():
-    def __init__(self, score, fromX, toX, curX, bps, activeNotes=set()):
+    def __init__(self, score, fromX, toX, curX, bps):
         self.score = score
         self.timeSig = self.score.timeSig
         self.keySig = self.score.keySig
-        self.fromX = fromX
-        self.toX = toX
-        self.curX = curX
+        self.activeNotes = set() # note_on exists before fromX (notes still playing)
+        self.fromX = float("-inf")
+        self.toX = float("-inf")
+        self.seek(fromX, toX)
         self.bps = bps
-        self.activeNotes = activeNotes # note_on exists before fromX (notes still playing)
+        self.curX = curX
     def seek(self, fromX, toX):
         """adjust current with bbst from self.score"""
         return NotImplemented
